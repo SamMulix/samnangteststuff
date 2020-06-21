e545fa61ddcd09b32787515409218e3789ee0e61
SamMulix <61001971+SamMulix@users.noreply.github.com>
2020-06-22 03:55:37 +0700

    lol

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.carousel').carousel({interval:10000})
+
+function login()
+{
+    location.href="register.html";
+}
+
+function register()
+{
+    location.href="login.html";
+}
+
+function loginbtn()
+{
+    location.href="login.html";
+}
+
+function signupbtn()
+{
+    location.href="register.html";
+}
